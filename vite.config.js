4e24d1d32032e354eba716d91c9be91f13cb0f9d
MF-Rayo <25manuelfm@gmail.com>
2026-08-11 21:06:27 -0500

    Fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: 'https://github.com/MF-Rayo/white-cat',
+  base: '/white-cat/',
   plugins: [
     react(),
     tailwindcss(),
