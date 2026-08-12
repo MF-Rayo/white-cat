71a622cb8d0a7ee1b97640fc3e1e465af059a1b2
MF-Rayo <25manuelfm@gmail.com>
2026-08-12 08:36:09 -0500

    hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { useState, useEffect } from "react"
 
 function App() {
   return (
-    <BrowserRouter basename="/white-cat/">
+    <BrowserRouter>
       <AppLayout />
     </BrowserRouter>
   )
